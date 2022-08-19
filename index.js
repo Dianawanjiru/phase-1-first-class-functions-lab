e5b4a98637bfd7b926e9ad57c1c605c7e9cc5c84
Diana <diana.wanjiru@student.moringaschool.com>
2022-08-19 20:56:45 +0300

    completed test

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+const returnFirstTwoDrivers = function (driverNumber){
+    return [driverNumber[0], driverNumber[1]]
+    
+}
+
+returnFirstTwoDrivers(drivers);
+
+const returnLastTwoDrivers = function (driverNumber){
+    return [driverNumber[driverNumber.length-2], driverNumber[driverNumber.length-1]]
+}
+returnLastTwoDrivers(drivers);
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier =  ride =>{
+    return function(fare){
+        return fare * ride
+
+    }
+}
+
+const newFare = createFareMultiplier(2);
+function fareDoubler (double){
+    return newFare(double);
+}
+
+const fareCalculator = createFareMultiplier(3);
+function fareTripler (triples){
+    return fareCalculator(triples);
+}
+
+function selectDifferentDrivers (drivers, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(drivers);
+
+}
